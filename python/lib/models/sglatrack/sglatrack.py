b506ea64f82e554537d115298e192fa70e97e347
chanyuan <whfjgir325@gmail.com>
2026-04-18 03:21:33 +0800

    add vit_CARE_relu6_fixed & vit_CARE_relu_fixed

--- python/lib/models/sglatrack/sglatrack.py
+++ python/lib/models/sglatrack/sglatrack.py
@@ -12,7 +12,9 @@ from lib.models.sglatrack.vit_square import vit_base_patch16_224 as vit_square_b
 from lib.models.sglatrack.vit_sima import vit_base_patch16_224 as vit_sima_base_patch16_224
 from lib.models.sglatrack.vit_CARE import vit_base_patch16_224 as vit_care_base_patch16_224
 from lib.models.sglatrack.vit_CARE_relu import vit_base_patch16_224 as vit_care_relu_base_patch16_224
+from lib.models.sglatrack.vit_CARE_relu_fixed import vit_base_patch16_224 as vit_care_relu_fixed_base_patch16_224
 from lib.models.sglatrack.vit_CARE_relu6 import vit_base_patch16_224 as vit_care_relu6_base_patch16_224
+from lib.models.sglatrack.vit_CARE_relu6_fixed import vit_base_patch16_224 as vit_care_relu6_fixed_base_patch16_224
 from lib.models.sglatrack.vit_CARE_relu_BN import vit_base_patch16_224 as vit_care_relu_bn_base_patch16_224
 from lib.models.sglatrack.vit_CARE_gelu import vit_base_patch16_224 as vit_care_gelu_base_patch16_224
 from lib.models.sglatrack.vit_MALA import vit_base_patch16_224 as vit_mala_base_patch16_224
@@ -154,8 +156,16 @@ def build_sglatrack(cfg, training=True):
         backbone = vit_care_relu_base_patch16_224(pretrained, drop_path_rate=cfg.TRAIN.DROP_PATH_RATE)
         hidden_dim = backbone.embed_dim
         patch_start_index = 1
+    elif cfg.MODEL.BACKBONE.TYPE == 'vit_care_relu_fixed_base_patch16_224':
+        backbone = vit_care_relu_fixed_base_patch16_224(pretrained, drop_path_rate=cfg.TRAIN.DROP_PATH_RATE)
+        hidden_dim = backbone.embed_dim
+        patch_start_index = 1
     elif cfg.MODEL.BACKBONE.TYPE == 'vit_care_relu6_base_patch16_224':
         backbone = vit_care_relu6_base_patch16_224(pretrained, drop_path_rate=cfg.TRAIN.DROP_PATH_RATE)
+        hidden_dim = backbone.embed_dim
+        patch_start_index = 1
+    elif cfg.MODEL.BACKBONE.TYPE == 'vit_care_relu6_fixed_base_patch16_224':
+        backbone = vit_care_relu6_fixed_base_patch16_224(pretrained, drop_path_rate=cfg.TRAIN.DROP_PATH_RATE)
         hidden_dim = backbone.embed_dim
         patch_start_index = 1
     elif cfg.MODEL.BACKBONE.TYPE == 'vit_care_relu_bn_base_patch16_224':
